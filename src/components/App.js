e7d4b740e00d0845ea39bdc3a299cb67415c8e2f
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-27 13:15:11 +0530

    submitting code-Sun Aug 27 2023 13:15:10 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,50 +3,61 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [itemName,setItemName]=useState("");
-  const [price,setprice]=useState('');
-  const [cart,setCart]=useState([]);
-
-  const addItem=()=>{
-    const newItem={
-      name:itemName,
-      price:price
-    };
-    setCart([...cart,newItem]);
-    setItemName("");
-    setprice("");
+  const [arr, setArr] = useState([{name:"Item 1",price:10},{name:"Item 2",price:20},{name:"Item 3",price:30}]);
+  let [values,setValues] = useState({});
+  function handleChange1(e){
+    setValues({...values,name:e.target.value});
+  }
+  function handleChange2(e){
+    setValues({...values,price:e.target.value});
+  }
+  function handleClick(){
+    if(arr.length === 0){
+      // console.log("hell");
+      let a = [];
+      a.push({name:values.name,price:values.price})
+      setArr(a);
+    }else{
+      setArr([...arr,{name:values.name,price:values.price}]);
+    }
   }
 
- const removeItem=(index)=>{
-  const update=cart.filter((_,i)=>i!=index)
-  setCart(update);
- }
+  function handleChild(e){
+    let item = e.target.parentElement.innerText.substr(0,7);
+    let ab = arr.filter((e)=>{
 
+      if(e.name[5] == item[5]){
+        return false;
+      }
+      return true;
+    });
+    setArr(ab);
 
-
+  }
   return (
-    <div className="parent">
-       <h1>Parent Component</h1>  
-       <label>Item Name:</label>
-       <input type="text" id="itemName" onChange={(e)=>setItemName(e.target.value)} value={itemName}/>
-       <label>Item Price:</label>
-       <input type="number" id="itemPrice" onChange={(e)=>setprice(e.target.value)} value={price}/> 
-       <button onClick={addItem}>Add Item</button>
-
-       <div className="child">
-        <h2>Child Component</h2>
-        {
-          cart.map((item,index)=>{
-            <li key={index}>
-              <span className="itemName">{item.name}</span>
-              <span className="itemPrice">{item.price}</span>
-              <button onClick={()=>removeItem(index)}>Remove</button>
-            </li>
-          })
-        }
-       </div>
+    <div>
+        {/* Do not remove the main div */}
+        <div className="parent">
+          <h1>Parent Component</h1>
+          <label htmlFor="">Item Name</label>
+          <input type="text"  id="itemName" onChange={handleChange1}/>
+          <label htmlFor="">Item Price</label>
+          <input type="number"  id="itemPrice" onChange={handleChange2}/>
+          <button onClick={handleClick}>Add Item</button>
+          <div className="child">
+            <h1>Child Component</h1>
+            <ul>
+            {
+              arr.map((e,i)=>{
+                return <li>{e.name} - ${e.price} <button onClick={handleChild}>Remove</button></li>    
+                
+              })
+            }
+            </ul>
+          </div>
+        </div>
     </div>
-  );
+  )
 }
 
 export default App
